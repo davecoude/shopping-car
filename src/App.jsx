76aeba5248c28fbcb8cea911fd86c159967d3f89
davecoude <diegoalejandrovanegas97@gmail.com>
2023-06-05 22:46:56 -0500

    Filtro de productos precio-categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react"
+import { Products } from "./components/Products.jsx"
+import { Header } from "./components/Header.jsx"
+import { products as initialProducts } from "./mocks/products.json"
 
 function App() {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'laptops',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return(
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
 
   return (
     <>
-      <h1>Shopping cart</h1>
+      <Header />
+      <Products products={filteredProducts} />
     </>
   )
 }
