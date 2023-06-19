62b8f8ac41d74a564e06f8760d262942bc49cdc5
Diego Vanegas <diegoalejandrovanegas97@gmail.com>
2023-06-19 13:03:54 -0500

    costum hook filtros y consumo Context Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { Products } from "./components/Products.jsx"
 import { Header } from "./components/Header.jsx"
 import { products as initialProducts } from "./mocks/products.json"
+import { FiltersContext } from "./context/filters.jsx"
 
-function App() {
-  const [products] = useState(initialProducts)
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
+function useFilters(products) {
+  // uso del estado global -> useContext
+  const { filters, setFilters } = useContext(FiltersContext)
 
   const filterProducts = (products) => {
     return products.filter(product => {
@@ -23,6 +21,15 @@ function App() {
   }
 
   const filteredProducts = filterProducts(products)
+
+  return { setFilters, filteredProducts }
+
+}
+
+function App() {
+  const [products] = useState(initialProducts)
+  const { setFilters, filteredProducts } = useFilters(products)
+  
 
   return (
     <>
