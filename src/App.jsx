9f8d59baa5644e025de3dbf104d7f2bd71cec326
Diego Vanegas <diegoalejandrovanegas97@gmail.com>
2023-06-20 19:40:55 -0500

    Contexto para carrito de compras

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Products } from "./components/Products.jsx"
 import { Header } from "./components/Header.jsx"
 import { products as initialProducts } from "./mocks/products.json"
 import { useFilters } from "./hooks/useFilters.js"
+import { Cart } from "./components/Cart.jsx"
+import { CartProvider } from "./context/cart.jsx"
 
 
 function App() {
@@ -13,10 +15,11 @@ function App() {
   
 
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
-    </>
+    </CartProvider>
   )
 }
 
